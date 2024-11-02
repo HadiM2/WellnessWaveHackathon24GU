bc9b81ee198dfcee7b65f3883cde91d89560bc28
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-11-02 14:57:53 -0700

    Working on app homepage

--- Hackathon2024/app.vue
+++ Hackathon2024/app.vue
@@ -5,25 +5,32 @@
       <v-app-bar>
         <v-list-item class="title-font">Wellness Wave 🌊</v-list-item>
         <v-spacer></v-spacer>
-        <v-app-bar-nav-icon
-          @click="menu = !menu"
-          class="custom-hamburger"
-        ></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click="menu = !menu"></v-app-bar-nav-icon>
       </v-app-bar>
       <v-navigation-drawer v-model="menu" location="right">
         <v-list>
           <v-list-item @click="router.push('/')">
-              <v-list-item-title class="font">Fit Tracker&nbsp;<v-icon size="x-small">mdi-weight-lifter</v-icon></v-list-item-title>
+            <v-list-item-title class="font"
+              >Fit Tracker&nbsp;<v-icon size="x-small"
+                >mdi-weight-lifter</v-icon
+              ></v-list-item-title
+            >
           </v-list-item>
           <br />
           <v-list-item @click="router.push('/journal')">
-
-              <v-list-item-title class="font">Journal&nbsp;<v-icon size="x-small">mdi-notebook</v-icon></v-list-item-title>
+            <v-list-item-title class="font"
+              >Journal&nbsp;<v-icon size="x-small"
+                >mdi-notebook</v-icon
+              ></v-list-item-title
+            >
           </v-list-item>
           <br />
           <v-list-item @click="router.push('/articleHub')">
-            
-              <v-list-item-title class="font">Articles &nbsp;<v-icon size="x-small">mdi-book-open-page-variant-outline</v-icon></v-list-item-title>
+            <v-list-item-title class="font"
+              >Articles &nbsp;<v-icon size="x-small"
+                >mdi-book-open-page-variant-outline</v-icon
+              ></v-list-item-title
+            >
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
@@ -41,31 +48,31 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-const modelValue = ref(false); 
+const modelValue = ref(false);
 
-const menu = ref(false); 
+const menu = ref(false);
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Poiret+One&display=swap");
 
-
 .title-font {
   font-family: "Poiret One", sans-serif;
   font-size: 18px;
-  font-weight:bolder;
+  font-weight: bolder;
+  color: black;
 }
 
 .font {
   font-family: "Poiret One", sans-serif;
   font-size: 18px;
-  font-weight:bold;
+  font-weight: bold;
 }
 
 /* Responsive styles */
 @media (max-width: 600px) {
   .custom-nav-button {
-    font-size: 14px; 
+    font-size: 14px;
   }
 }
 </style>
